b29633f5c5cf777061e4696dccc55077af867169
Mr. Goferito <goferito@gmail.com>
2016-11-10 19:04:06 +0100

    Implement getLeagues()

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-const secrets = require('./secrets')
+const request = require('request')
+
+class RF {
+
+  constructor (key) {
+
+    const rootUri = 'http://apiclient.resultados-futbol.com/scripts/api/api.php'
+
+    this.reqUri = `${rootUri}?key=${key}&format=json`
+
+  }
+
+  /**
+   * @return {Promise}
+   */
+  getLeages () {
+    return new Promise((resolve, reject) => {
+      request({
+        uri: this.reqUri + '&req=leagues',
+        method: "GET"
+      }, (err, res, body) => {
+        if (err) return reject(err)
+
+        if (res && res.statusCode !== 200) {
+          return reject(new Error(body))
+        }
+
+        resolve(typeof body === 'string' ? JSON.parse(body) : body)
+      })
+    })
+  }
+
+}
+
+module.exports = RF
---
+++ test/integration.js
@@ -0,0 +1,27 @@
+
+const secrets = require('../secrets')
+const RF = require('../index')
+
+describe('RF API', () => {
+
+  const rf = new RF(secrets.apiKey)
+
+  it('creates the RF object', () => {
+    expect(rf).to.be.ok
+    expect(rf.reqUri).to.have.string(secrets.apiKey)
+  })
+
+  it('gets the leages', (done) => {
+    rf.getLeages().then((leagues) => {
+
+      expect(leagues).to.be.ok
+      expect(typeof leagues).to.equal('object')
+      expect(leagues.league).to.have.length.above(1000)
+      done()
+      
+    })
+    .catch(done)
+  })
+
+})
+
